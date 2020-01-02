dc72413af37d8869476ceaefd76108a0bd2934ba
AngryPanda <924193579guge@gmail.com>
2020-01-02 09:45:07 +0800

    v1.0

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var mySwiper = new Swiper ('.swiper-container', {
+        
+        loop: true,
+        slidesPerView: 'auto',
+        
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+      })
+}
